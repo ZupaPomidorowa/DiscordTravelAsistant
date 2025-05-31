1bb9ed0ec2409ccc0db4e0df876a3c1970c8009b
D <daria.danieluk@gmail.com>
2025-05-31 23:06:28 +0200

    Second commit

---
+++ attractions.py
@@ -0,0 +1,99 @@
+import googlemaps
+from discord.ext import commands
+import polyline
+from geopy import distance
+import pandas as pd
+import os
+
+gmaps_key = os.environ.get('GMAPS_KEY')
+
+gmaps = googlemaps.Client(key=gmaps_key)
+
+def check_address(address):
+    address_val = gmaps.addressvalidation(address, regionCode='PL')
+
+    verdict = address_val['result']['verdict']
+    city = address_val['result']['address']['postalAddress']['locality'].lower()
+
+    if verdict.get('hasUnconfirmedComponents') == True:
+        raise commands.CommandError(f'Address is not confirmed. Remember to write address in "" e.g. "plac Defilad 1 Warszawa"')
+
+    if city != 'warszawa' and city != 'warsaw':
+        raise commands.CommandError(f'Only addresses in Warsaw are allowed')
+
+    lat = address_val['result']['geocode']['location']['latitude']
+    long = address_val['result']['geocode']['location']['longitude']
+
+    return (lat, long)
+
+
+def find_attractions(latlong_origin, latlong_dest, num):
+    travel_modes = ['driving', 'walking', 'bicycling', 'two-wheeler', 'transit']
+    route = gmaps.directions(latlong_origin, latlong_dest, mode=travel_modes[1])
+    overview_polyline = route[0]["overview_polyline"]["points"]
+    overview_polyline = polyline.decode(overview_polyline)
+
+    places_data = {'Name': [], 'Id': [], 'Lat': [], 'Long': [], 'Rating': [], 'User_rating': []}
+    found_places = set()
+
+    for _, point in enumerate(overview_polyline):
+
+        radius = 500
+
+        places = gmaps.places(query='attraction', location=point, radius=radius)
+
+        for i, data in enumerate(places['results']):
+            place_id = data['place_id']
+            if place_id in found_places:
+                continue
+
+            lat = data['geometry']['location']['lat']
+            lng = data['geometry']['location']['lng']
+            real_distance = distance.distance(point, (lat, lng)).meters
+            if real_distance > radius:
+                continue
+
+            found_places.add(place_id)
+
+            name = data['name']
+            rating = data.get('rating', 0)
+            user_rating_total = data.get('user_ratings_total', 0)
+
+            places_data['Name'].append(name)
+            places_data['Id'].append(place_id)
+            places_data['Lat'].append(lat)
+            places_data['Long'].append(lng)
+            places_data['Rating'].append(rating)
+            places_data['User_rating'].append(user_rating_total)
+
+    df = pd.DataFrame(places_data)
+
+    df_sorted = df.sort_values(by=['Rating', 'User_rating'], ascending=False).head(num)
+
+    attractions_latlong = list(zip(df_sorted['Lat'], df_sorted['Long']))
+    names = list(df_sorted['Name'])
+
+    optimize_route = gmaps.directions(latlong_origin, latlong_dest, mode=travel_modes[1], waypoints=attractions_latlong,
+                                      optimize_waypoints=True)
+    order = optimize_route[0]['waypoint_order']
+
+    optimized_attractions = [attractions_latlong[i] for i in order]
+    optimized_names = [names[i] for i in order]
+
+    return optimized_attractions, optimized_names
+
+
+def create_url(latlong_origin, latlong_dest, attractions):
+
+    url = "https://www.google.com/maps/dir/?api=1"
+    origin = "&origin=" + str(latlong_origin[0]) + "," + str(latlong_origin[1])
+    dest = "&destination=" + str(latlong_dest[0]) + "," + str(latlong_dest[1])
+    travel_modes = ['driving', 'walking', 'bicycling', 'two-wheeler', 'transit']
+    travel_mode = "&travelmode=" + travel_modes[1]
+
+    if attractions:
+        waypoints = "&waypoints=" + "|".join([f"{lat},{lng}" for lat, lng in attractions])
+    else:
+        waypoints = ""
+
+    return url + origin + dest + waypoints + travel_mode
---
+++ bot.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+from attractions import *
+import os
+
+discord_key = os.environ.get('DISCORD_KEY')
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+@bot.command(name='find_route', help='Find the route between two locations and finds attractions on way in Warsaw.\nUsage: !find_route <origin> <destination> <number_of_attractions>')
+async def find_route(ctx, origin, dest, num: int):
+
+    latlong_origin = check_address(origin)
+    latlong_dest = check_address(dest)
+
+    await ctx.send('Got it! Finding a route with attractions')
+
+    top_attractions, attraction_names = find_attractions(latlong_origin, latlong_dest, num)
+
+    url = create_url(latlong_origin, latlong_dest, top_attractions)
+
+    attraction_names_str =  '\n'.join([f'{i + 1}. {name}' for i, name in enumerate(attraction_names)])
+    await ctx.send(f"Route with {num} best attractions: \n{attraction_names_str}")
+
+    await ctx.send(f'Link to your route: {url}')
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Missing arguments. Usage: !find_route <origin> <destination> <number_of_attractions>')
+    if isinstance(error, commands.CommandError):
+        await ctx.send(error)
+    if isinstance(error, commands.BadArgument):
+        await ctx.send("The number of attractions parameter is not a number")
+
+bot.run(discord_key)
+
+
